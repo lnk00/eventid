59a162bfba95b87f279b2d2ff02f78b9ce60e1a3
Damien Dumontet <damien.dumontet@protonmail.com>
2022-11-29 06:22:05 -0300

    init commit

---
+++ src/root.tsx
@@ -0,0 +1,21 @@
+import { component$, useStyles$ } from '@builder.io/qwik';
+import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+
+import globalStyles from './global.css?inline';
+
+export default component$(() => {
+  useStyles$(globalStyles);
+
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
+      <body lang="en">
+        <RouterOutlet />
+        <ServiceWorkerRegister />
+      </body>
+    </QwikCityProvider>
+  );
+});
